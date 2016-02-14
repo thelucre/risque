7981ee5d0244302545b0b5bcb623030138c6797a
Eric Howard <howard.eric.m@gmail.com>
2016-02-14 10:59:16 -0800

    global user context for views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,28 +3,33 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Auth, View;
 
 class AppServiceProvider extends ServiceProvider
 {
 		/**
-		 * Bootstrap any application services.
-		 *
-		 * @return void
-		 */
-		public function boot()
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		// Global user context
+		View::composer('*', function($view)
 		{
-				//
-		}
+			$view->with('user', Auth::user());
+		});
+	}
 
-		/**
-		 * Register any application services.
-		 *
-		 * @return void
-		 */
-		public function register()
-		{
-			if ($this->app->environment() == 'local') {
-				$this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
-			}
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		if ($this->app->environment() == 'local') {
+			$this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
 		}
+	}
 }
